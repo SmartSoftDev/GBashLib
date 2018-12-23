ce4622714fd4b21a7f7878456f74ef9cac460098
Gheorghe Stancov <gheorghe.stancov@gmail.com>
2018-12-23 18:01:39 +0100

    add --delete function to tpl and add some systemd functions

---
+++ src/libs/systemd.lib.sh
@@ -0,0 +1,26 @@
+. $(gbl log)
+
+function systemd_create_or_update_from_tpl(){
+    local service_name="$1"
+    local tpl_file_path="$2"
+    shift
+    shift
+    # all other arguments will be passed to -v of TPL command
+
+    local service_file=${service_name}.service
+    local sysd_file=/lib/systemd/system/$service_file
+    if [ -f $sysd_file ];then
+        log "stop $service_file"
+        sudo systemctl stop $service_file
+    fi
+    log "generate $sysd_file"
+    local v_cmd="-v"
+    if [ "x$@" == "x" ] ; then v_cmd="" ; fi
+
+    sudo tpl -i $tpl_file_path \
+            -o $sysd_file \
+            $v_cmd $@
+    sudo systemctl daemon-reload
+    sudo systemctl enable $service_file
+    sudo systemctl start $service_file
+}
