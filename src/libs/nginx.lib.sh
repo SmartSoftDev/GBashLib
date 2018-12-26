831c22df80718f2bcc5bb9037aa5f4492c30de04
Gheorghe Stancov <gheorghe.stancov@gmail.com>
2018-12-26 20:32:31 +0100

    add uidgen functionality

---
+++ src/libs/nginx.lib.sh
@@ -0,0 +1,21 @@
+. $(gbl log)
+
+function nginx_install_service(){
+    local service_name="$1"
+    local tpl_path="$2"
+    shift
+    shift
+    # all other arguments will be passed to -v of TPL command
+
+    local NGINX_CFG=/etc/nginx/sites-available/gen.$service_name.cfg
+    log "generate nginx config: $NGINX_CFG"
+    local v_cmd="-v"
+    if [ "$#" -eq 0 ] ; then v_cmd="" ; fi
+
+    #config nginx
+    sudo tpl -i $tpl_path -o $NGINX_CFG $v_cmd $@
+    sudo ln -sf $NGINX_CFG /etc/nginx/sites-enabled/
+    sudo nginx -t || fatal "NGINX configuration failed"
+    sudo systemctl restart nginx.service
+
+}
