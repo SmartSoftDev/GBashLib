9b5b02a98861bc20915c4c819d6839a772bf110e
ghe0rghe <gheorghe.stancov@gmail.com>
2021-03-05 17:03:47 +0100

    build PUML files only if they change

--- src/libs/reqs.lib.sh
+++ src/libs/reqs.lib.sh
@@ -25,9 +25,13 @@ function reqs_build_one(){
 	local puml_exec="$(readlink -e "$this_dir/../../tools/plantuml.jar")"
 	echo "Start building document $path"
 	for pfile in $(find -L $path -name "*.puml" -type f) ; do
-		java -jar $puml_exec $pfile || { echo "failed to convert PUML to PNG: '$pfile'" ; return 1 ; }
-		mv "${pfile:0:(-5)}.png" $pfile.png
-		echo "Converted puml file: $pfile.png"
+		if [ "$pfile" -nt "$pfile.png" ] ;then
+			java -jar $puml_exec $pfile || { echo "failed to convert PUML to PNG: '$pfile'" ; return 1 ; }
+			mv "${pfile:0:(-5)}.png" $pfile.png
+			echo "Converted puml file: $pfile.png"
+		else
+			echo "$pfile.png already up to date"
+		fi
 	done
 	$SPHINXBUILD -M $target $path ${path}_build ${SPHINXOPTS}
 }
