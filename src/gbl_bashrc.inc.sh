d853a21ce224a3e04aea411d67f5514b7a77bdfd
ghe0rghe <gs@smartsoftdev.eu>
2021-08-29 22:07:19 +0200

    improve console ordering (order by syspath, always predictable)

--- src/gbl_bashrc.inc.sh
+++ src/gbl_bashrc.inc.sh
@@ -276,6 +276,16 @@ function sd(){
 
 # 's' ssh shortcut !
 
+function _list_sorted_console(){
+    {
+        local i reali
+        for i in $(ls /sys/class/tty/ | grep ttyUSB) ; do
+            reali=$(realpath /sys/class/tty/$i)
+            echo "$reali"
+        done
+     } | sort
+}
+
 _gbl_my_console_usage(){
     echo -e "usage to console: $COLOR_GREEN c CONSOLE_ALIAS$COLOR_NONE\n\
 usage to set: $COLOR_GREEN c set CONSOLE_ALIAS IP$COLOR_NONE\n\
@@ -283,6 +293,11 @@ usage to del: $COLOR_GREEN c del CONSOLE_ALIAS $COLOR_NONE\n\
 usage to list: $COLOR_GREEN c $COLOR_NONE or $COLOR_GREEN c list$COLOR_NONE "
 
     v list -dt console | column -t -n
+    i=0
+    for c in  $(_list_sorted_console) ; do
+        echo "$i -> $(basename $c) : $(dirname $(dirname $(dirname $c)))"
+        i=$((i+1))
+    done
 }
 
 
@@ -305,7 +320,33 @@ _gbl_my_console(){
             _gbl_my_console_usage
             return 1
         fi
-        echo "set console: $alias = $full_console"
+        # we need to convert the number to sys_class_path
+        local dev_path="$full_console"
+        local baudrate=115200
+
+        local arr=(${full_console//@/ })
+        if [ "${#arr[@]}" == "2" ] ; then
+            dev_path=${arr[0]}
+            baudrate=${arr[1]}
+        fi
+        local i=0
+        local ip=
+        for c in  $(_list_sorted_console) ; do
+            if [ "$i" == "$dev_path" ] ; then
+                ip="$(dirname $(dirname $(dirname $c)))"
+                break
+            fi
+            i=$((i+1))
+        done
+        [ -z "$ip" ] && {
+            echo -e "Could not find the console port # $dev_path \n\n"
+            _gbl_my_console_usage
+            return 1
+        }
+
+        echo "set console to sys_path: # $dev_path -> $ip "
+        full_console="$ip@$baudrate"
+        echo "set it to $alias = $full_console"
         v set -t console "$alias=$full_console"
         return 0;
     ;;
@@ -322,11 +363,27 @@ _gbl_my_console(){
     }
 
     local ip=$(v get -t console "$alias" )
-    [ "$ip" == "" ] && {
-        ip=($(v list -t console -n | grep "$alias"))
+    if [ "$ip" == "" ] ; then
+        ip=()
+        if [ ${#alias} -gt 1 ] ; then
+            # we search for allias only when is more than 1 character
+            ip=($(v list -t console -n | grep "$alias"))
+        fi
         if [ ${#ip[@]} == 0 ] ; then
-            echo -e "console is $COLOR_RED NOT-SET $COLOR_NONE use '$COLOR_GREEN c set $alias DEV_PATH@BAUDRATE$COLOR_NONE'"
-            return 1
+            if [ -n "$alias" ] ; then
+                echo "choised console #$alias"
+                i=0
+               for c in  $(_list_sorted_console) ; do
+                    if [ "$i" == "$alias" ] ; then
+                        ip="$(dirname $(dirname $(dirname $c)))"
+                        break
+                    fi
+                    i=$((i+1))
+                done
+            else
+                echo -e "console is $COLOR_RED NOT-SET $COLOR_NONE use '$COLOR_GREEN c set $alias DEV_PATH@BAUDRATE$COLOR_NONE'"
+                return 1
+            fi
         elif (( ${#ip[@]} > 1 )) ; then
             echo -e "console $alias is ambigues: (${ip[@]})"
             return 1
@@ -335,13 +392,15 @@ _gbl_my_console(){
             alias="$ip"
             ip=$(v get -t console "$ip" )
         fi
-    }
+    fi
+    echo "got $ip"
+
     if [ -z "$ip" ] ; then
         #wrong arguments
         return 1
     fi
 
-    # let's extract /dev/ttyUSB0@115200
+    # let's extract baudrate from SYS_DEV_PATH@115200
     local dev_path="$ip"
     local baudrate=115200
     local arr=(${ip//@/ })
@@ -349,6 +408,15 @@ _gbl_my_console(){
         dev_path=${arr[0]}
         baudrate=${arr[1]}
     fi
+    # let's convert the sys_DEV_PATH to read dev_path
+    local real_sys_path=$(_list_sorted_console | grep "$dev_path")
+    if [ -z "$real_sys_path" ] ; then
+        echo -e "requested sys_path does not exist: $dev_path\n\n"
+        _gbl_my_console_usage
+        return 1
+    fi
+    dev_path=/dev/$(basename $real_sys_path)
+
     log "Starting  screen console to $dev_path ($baudrate): to exit Ctrl + a then k"
     sudo screen "$dev_path" "$baudrate"
 
