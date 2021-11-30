187d05a9602d20792bd7eb52c2c1a18e1250ee1b
ghe0rghe <gs@smartsoftdev.eu>
2021-11-30 08:29:03 +0100

    add bash.rc list command

--- src/bin/bash.rc.sh
+++ src/bin/bash.rc.sh
@@ -16,6 +16,20 @@ gblcmd_remove(){
     tpl -i $G_BASH_LIB/tpls/bashrc.tpl -r -I "$brc_lib" -d -o $dest
 }
 
+gblcmd_list(){
+    for i in $(cat $dest | grep "#G_BASH_LIB_bashrc.inc" | grep -v "_END")
+    do
+        local lib_name=${i:22}
+        [ "$lib_name" != "" ] && echo $lib_name
+    done
+    echo "Available for in GBashLib:"
+    for i in $(ls $G_BASH_LIB/bash.rc_libs/)
+    do
+        local lib_name=${i:0:-7}
+        [ "$lib_name" != "" ] && echo -e "\t$lib_name"
+    done
+}
+
 #run G_BASH_LIB
 [ -z "$G_BASH_LIB" ] && { echo -e "!!!!\nFATAL ERROR: G_BASH_LIB variable is not set!\nAdd it in ~/.bashrc or use export G_BASH_LIB=path_to_the_lib"; exit 1; }
 . "$G_BASH_LIB/GBashLib.sh"
