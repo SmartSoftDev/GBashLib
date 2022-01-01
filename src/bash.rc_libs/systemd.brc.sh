51088fc922e44fa85fd169534da61cb4cc8fd94a
ghe0rghe <gs@smartsoftdev.eu>
2022-01-01 17:24:54 +0100

    add sudo to systemctl shortcuts

--- src/bash.rc_libs/systemd.brc.sh
+++ src/bash.rc_libs/systemd.brc.sh
@@ -1,7 +1,7 @@
 #systemd shortcuts
-alias dctl='systemctl'
-alias dst='systemctl status'
-alias dstart='systemctl start'
-alias dstop='systemctl stop'
-alias drestart='systemctl restart'
+alias dctl='sudo systemctl'
+alias dst='sudo systemctl status'
+alias dstart='sudo systemctl start'
+alias dstop='sudo systemctl stop'
+alias drestart='sudo systemctl restart'
 alias dlog='journalctl'
