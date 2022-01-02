423752fdf8c72e941ce44a979ea1c8c24d683feb
ghe0rghe <gs@smartsoftdev.eu>
2022-01-02 07:53:31 +0100

    change aliases for systemd shortcuts

--- src/bash.rc_libs/systemd.brc.sh
+++ src/bash.rc_libs/systemd.brc.sh
@@ -1,7 +1,7 @@
-#systemd shortcuts
-alias dctl='sudo systemctl'
-alias dst='sudo systemctl status'
-alias dstart='sudo systemctl start'
-alias dstop='sudo systemctl stop'
-alias drestart='sudo systemctl restart'
-alias dlog='journalctl'
+# systemd shortcuts
+alias sc='sudo systemctl'
+alias scst='systemctl status'
+alias scstart='sudo systemctl start'
+alias scstop='sudo systemctl stop'
+alias screstart='sudo systemctl restart'
+alias jc='journalctl'
