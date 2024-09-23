574274ac40eddba01f253a6eaa54726795d1e239
Gheorghi Stancov <gs@smartsoftdev.eu>
2024-09-23 21:16:57 +0200

    improve instalation on ubuntu 24

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
+SCRIPT_DIR=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
+REPO=$(readlink -f $SCRIPT_DIR/../../)
+. $REPO/src/libs/osDetection.lib.sh
+
 sudo -H apt install -y python3 python3-pip bsdmainutils
-sudo -H pip3 install --upgrade pyyaml tabulate python-gnupg
+if is_os ubuntu_24 ; then
+    echo "FYI: Setting break-system-packages = true for PIP in /etc/pip.conf"
+    sudo cp $REPO/src/scripts/confs/ubuntu_24_pip.conf /etc/pip.conf
+    sudo -H apt install -y python3-yaml
+    sudo -H pip3 install --upgrade tabulate python-gnupg
+else
+    sudo -H pip3 install --upgrade pyyaml tabulate python-gnupg
+fi
